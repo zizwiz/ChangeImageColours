4092d0ea39379ef2f7beb2d1ce3fcbdcce327bb5
zizwiz <zizwiz@engineer.com>
2024-09-17 20:17:18 +0100

    Initial upload of files.
    Initial upload of files.

---
+++ ColourChanger/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace ColourChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_help = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(1001, 593);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(141, 42);
+            this.btn_close.TabIndex = 0;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_help
+            // 
+            this.btn_help.Location = new System.Drawing.Point(83, 647);
+            this.btn_help.Name = "btn_help";
+            this.btn_help.Size = new System.Drawing.Size(141, 42);
+            this.btn_help.TabIndex = 1;
+            this.btn_help.Text = "Help";
+            this.btn_help.UseVisualStyleBackColor = true;
+            this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1296, 756);
+            this.Controls.Add(this.btn_help);
+            this.Controls.Add(this.btn_close);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Colour Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_help;
+    }
+}
+
---
+++ ColourChanger/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using help_about;
+
+namespace ColourChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text += " : v" + Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
+
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_help_Click(object sender, EventArgs e)
+        {
+            Help_Form help = new Help_Form();
+            help.Show();
+        }
+    }
+}
